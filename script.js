fcb4cccddd5612603c7d309f1eb408d93c7a5517
estefhanyarraez-ux <estefhany.arraez@escola.pr.gov.br>
2026-09-09 17:24:41 -0300

    alterei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const botao=document.querySelector("#botaoMenagem");
+const mensagem =document.querySelector("#mensagemInterativa");
+botao.addEventListener("click",mostrarMensagem);
+function mostrarMensagem(){
+    mensagem.textContent =
+    "você acabou de criar uma interação com JavaScript"
+}
