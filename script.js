cbe1c8f088f179189643a9ce245ee866398eb1f0
estefhanyarraez-ux <estefhany.arraez@escola.pr.gov.br>
2026-09-16 16:42:25 -0300

    Ajustes nas imagens

--- script.js
+++
@@ -1,7 +0,0 @@
-const botao=document.querySelector("#botaoMensagem");
-const mensagem =document.querySelector("#mensagemInterativa");
-botao.addEventListener("click",mostrarMensagem);
-function mostrarMensagem(){
-    mensagem.textContent =
-    "você acabou de criar uma interação com JavaScript"
-}
