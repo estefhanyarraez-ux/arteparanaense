fb3f34d24e70a85e46a0e4c91ad0c061f6b146b4
estefhanyarraez-ux <estefhany.arraez@escola.pr.gov.br>
2026-09-16 17:33:29 -0300

    atualizei o codigo

---
+++ script.js
@@ -0,0 +1,16 @@
+conts botoesCurtir= document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir) {
+    let curtiu=false;
+    botaoCurtir.addEventListener("click",curtir);
+    function curtir(){
+        const contador= botaoCurtir.querySelector("span");
+        if(curtiu===false){
+            contador.textContent++;
+            curtiu=true;}
+            else{
+                contador.textContent--;
+                curtiu=false;
+        }
+}
+    
+});
